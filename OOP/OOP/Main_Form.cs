4741ee3d78baed5c0769af89d2b5be8bd3b732d2
DRed <dima.redkin.1999@gmail.com>
2019-03-29 18:42:36 +0500

    Now SaveButton exports all labels and grids to Excel file. Cells with borders, labels are bold. Fixed view of rational values in excel

--- OOP/OOP/Main_Form.cs
+++ OOP/OOP/Main_Form.cs
@@ -2,7 +2,10 @@
 using System.Net;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
-using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+using System.Globalization;
+//using Application = Microsoft.Office.Interop.Excel.Application;
 
 namespace OOP
 {
@@ -80,9 +83,120 @@ namespace OOP
 
         }
 
+        private void FillExcelTable()
+        {
+
+        }
+
         private void fifth_dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private static void MakeBordetForTableCells(Excel.Application ExcelApp, Excel.Worksheet ws, int currentRow, int rowCount, int colCount, int startOfColumn = 1)
+        {
+            Excel.Range tableRange = ws.Range[ExcelApp.Cells[currentRow, startOfColumn], ExcelApp.Cells[currentRow + rowCount, colCount]];
+            tableRange.Borders.Color = System.Drawing.Color.Black.ToArgb();
+        }
+
+        private void AddLabelToTable(Excel.Application ExcelApp, ref int currentRow,
+            Label label, int numberOfWorksheet = 1)
+        {
+            var ws = (Excel.Worksheet)ExcelApp.Worksheets[numberOfWorksheet];
+
+            var startOfRow = 1;
+
+            ExcelApp.Cells[currentRow, startOfRow] = label.Text.ToString();
+            MakeLabelBold(ExcelApp, ws, currentRow);
+            currentRow++;
+        }
+
+        private static void MakeLabelBold(Excel.Application ExcelApp, Excel.Worksheet ws, int currentRow, int startOfColumn = 1)
+        {
+            Excel.Range LabelRange = ws.Range[ExcelApp.Cells[currentRow, startOfColumn], ExcelApp.Cells[currentRow, startOfColumn]];
+            LabelRange.Font.Bold = true;
+        }
+
+        private void AddGridToTable(Excel.Application ExcelApp, ref int currentRow,
+            DataGridView dataGridView, int numberOfWorksheet = 1)
+        {
+            var ws = (Excel.Worksheet)ExcelApp.Worksheets[numberOfWorksheet];
+
+            var rowCount = dataGridView.RowCount;
+            var colCount = dataGridView.ColumnCount;
+
+            MakeBordetForTableCells(ExcelApp, ws, currentRow, rowCount, colCount);
+
+            //storing headers
+            for (int i = 0; i < dataGridView.ColumnCount; i++)
+            {
+                ExcelApp.Cells[currentRow, i + 1] = dataGridView.Columns[i].HeaderText;
+            }
+            currentRow++;
+
+            //storing every cell to excel sheet
+            for (int i = 0; i < dataGridView.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView.ColumnCount; j++)
+                {
+                    ExcelApp.Cells[i + currentRow, j + 1] = dataGridView[j, i].Value;
+                }
+            }
+
+            currentRow += dataGridView.RowCount;
+        }
+
+        private List<Label> PutTogetherLabels()
+        {
+            var labelList = new List<Label>();
+            labelList.Add(label1);
+            labelList.Add(label2);
+            labelList.Add(label3);
+            labelList.Add(label4);
+            labelList.Add(label5);
+            labelList.Add(label6);
+            labelList.Add(label7);
+            labelList.Add(label8);
+            labelList.Add(label9);
+
+            return labelList;
+        }
+
+        private List<DataGridView> PutTogetherDataGridViews()
+        {
+            var gridList = new List<DataGridView>();
+            gridList.Add(first_dataGridView);
+            gridList.Add(second_dataGridView);
+            gridList.Add(third_dataGridView);
+            gridList.Add(fourth_dataGridView);
+            gridList.Add(fifth_dataGridView);
+            gridList.Add(sixth_dataGridView);
+            gridList.Add(seventh_dataGridView);
+            gridList.Add(eighth_dataGridView);
+            gridList.Add(ninth_dataGridView);
+            gridList.Add(tenth_dataGridView);
+
+            return gridList;
+        }
+
+        private void FillTables(Excel.Application ExcelApp)
+        {
+            ExcelApp.Columns.ColumnWidth = 30;
+
+            var currentRow = 1;
+
+            var gridList = PutTogetherDataGridViews();
+            var labelList = PutTogetherLabels();
+
+            for (int i = 0; i < labelList.Count; i++)
+            {
+                AddLabelToTable(ExcelApp, ref currentRow, labelList[i]);
+                AddGridToTable(ExcelApp, ref currentRow, gridList[i]);
+                currentRow += 2;
+            }
+
+            AddGridToTable(ExcelApp, ref currentRow, gridList[9]);
+            currentRow += 2;
         }
 
         private void ExportToExcel_Click(object sender, EventArgs e)
@@ -95,25 +209,10 @@ namespace OOP
 
             if (SaveToExcelDialog.ShowDialog() != DialogResult.Cancel)
             {
-                var ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+                var ExcelApp = new Excel.Application();
                 ExcelApp.Application.Workbooks.Add(Type.Missing);
 
-                ExcelApp.Columns.ColumnWidth = 20;
-
-                //storing headers
-                for (int i = 1; i < first_dataGridView.ColumnCount + 1; i++)
-                {
-                    ExcelApp.Cells[1, i] = first_dataGridView.Columns[i - 1].HeaderText;
-                }
-
-                //storing every cell to excel sheet
-                for (int i = 0; i < first_dataGridView.RowCount; i++)
-                {
-                    for (int j = 0; j < first_dataGridView.ColumnCount; j++)
-                    {
-                        ExcelApp.Cells[i + 2, j + 1] = first_dataGridView[j, i].Value.ToString();
-                    }
-                }
+                FillTables(ExcelApp);
 
                 ExcelApp.ActiveWorkbook.SaveCopyAs(SaveToExcelDialog.FileName.ToString());
                 ExcelApp.ActiveWorkbook.Saved = true;
