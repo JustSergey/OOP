045415141cb0ae3e62627c83b66135daab083681
DRed <dima.redkin.1999@gmail.com>
2019-03-31 16:58:41 +0500

    Encapsulated Excel manipulations

---
+++ OOP/OOP/ExcelManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace OOP
+{
+    public class ExcelManager
+    {
+        public static List<Label> LabelList { get; set; }
+        public static List<DataGridView> GridList { get; set; }
+        public static SaveFileDialog Dialog { get; set; }
+
+        private static Excel.Application ExcelApp;
+
+        public static void ExportToExcel()
+        {
+            Dialog.InitialDirectory = "C:";
+            Dialog.Title = "Save as Excel File";
+            Dialog.FileName = "";
+            Dialog.Filter =
+                "Excel Files(2003)|*.xls|Excel Files(2007)|*.xlsx|Excel Files(2013)|*.xlsx";
+
+            if (Dialog.ShowDialog() != DialogResult.Cancel)
+            {
+                CreateAndFillExcelApp();
+            }
+        }
+
+        private static void CreateAndFillExcelApp()
+        {
+            ExcelApp = new Excel.Application();
+            ExcelApp.Application.Workbooks.Add(Type.Missing);
+
+            FillTables();
+
+            ExcelApp.ActiveWorkbook.SaveCopyAs(Dialog.FileName.ToString());
+            ExcelApp.ActiveWorkbook.Saved = true;
+            ExcelApp.Quit();
+        }
+
+        private static void FillTables()
+        {
+            ExcelApp.Columns.ColumnWidth = 30;
+
+            var currentRow = 1;
+
+            for (int i = 0; i < LabelList.Count; i++)
+            {
+                AddLabelToTable(ref currentRow, LabelList[i]);
+                AddGridToTable(ref currentRow, GridList[i]);
+                currentRow += 2;
+            }
+
+            AddGridToTable(ref currentRow, GridList[9]);
+            currentRow += 2;
+        }
+
+        private static void AddLabelToTable(ref int currentRow,
+            Label label, int numberOfWorksheet = 1)
+        {
+            var ws = (Excel.Worksheet)ExcelApp.Worksheets[numberOfWorksheet];
+
+            var startOfRow = 1;
+
+            ExcelApp.Cells[currentRow, startOfRow] = label.Text.ToString();
+            MakeLabelBold(ws, currentRow);
+            currentRow++;
+        }
+
+        private static void AddGridToTable(ref int currentRow,
+            DataGridView dataGridView, int numberOfWorksheet = 1)
+        {
+            var ws = (Excel.Worksheet)ExcelApp.Worksheets[numberOfWorksheet];
+
+            var rowCount = dataGridView.RowCount;
+            var colCount = dataGridView.ColumnCount;
+
+            MakeBordetForTableCells(ws, currentRow, rowCount, colCount);
+
+            //storing headers
+            for (int i = 0; i < dataGridView.ColumnCount; i++)
+            {
+                ExcelApp.Cells[currentRow, i + 1] = dataGridView.Columns[i].HeaderText;
+            }
+            currentRow++;
+
+            //storing every cell to excel sheet
+            for (int i = 0; i < dataGridView.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView.ColumnCount; j++)
+                {
+                    ExcelApp.Cells[i + currentRow, j + 1] = dataGridView[j, i].Value;
+                }
+            }
+
+            currentRow += dataGridView.RowCount;
+        }
+
+        private static void MakeLabelBold(Excel.Worksheet ws, int currentRow, int startOfColumn = 1)
+        {
+            Excel.Range LabelRange = ws.Range[ExcelApp.Cells[currentRow, startOfColumn], ExcelApp.Cells[currentRow, startOfColumn]];
+            LabelRange.Font.Bold = true;
+        }
+
+        private static void MakeBordetForTableCells(Excel.Worksheet ws, int currentRow, int rowCount, int colCount, int startOfColumn = 1)
+        {
+            Excel.Range tableRange = ws.Range[ExcelApp.Cells[currentRow, startOfColumn], ExcelApp.Cells[currentRow + rowCount, colCount]];
+            tableRange.Borders.Color = System.Drawing.Color.Black.ToArgb();
+        }
+    }
+}
--- OOP/OOP/Main_Form.cs
+++ OOP/OOP/Main_Form.cs
@@ -75,131 +75,49 @@ namespace OOP
                 DataManager.Serialize(Tables, TableFileName);
         }
 
-        private static void MakeBordetForTableCells(Excel.Application ExcelApp, Excel.Worksheet ws, int currentRow, int rowCount, int colCount, int startOfColumn = 1)
+        private void ExportToExcel_Click(object sender, EventArgs e)
         {
-            Excel.Range tableRange = ws.Range[ExcelApp.Cells[currentRow, startOfColumn], ExcelApp.Cells[currentRow + rowCount, colCount]];
-            tableRange.Borders.Color = System.Drawing.Color.Black.ToArgb();
-        }
-
-        private void AddLabelToTable(Excel.Application ExcelApp, ref int currentRow,
-            Label label, int numberOfWorksheet = 1)
-        {
-            var ws = (Excel.Worksheet)ExcelApp.Worksheets[numberOfWorksheet];
-
-            var startOfRow = 1;
-
-            ExcelApp.Cells[currentRow, startOfRow] = label.Text.ToString();
-            MakeLabelBold(ExcelApp, ws, currentRow);
-            currentRow++;
-        }
-
-        private static void MakeLabelBold(Excel.Application ExcelApp, Excel.Worksheet ws, int currentRow, int startOfColumn = 1)
-        {
-            Excel.Range LabelRange = ws.Range[ExcelApp.Cells[currentRow, startOfColumn], ExcelApp.Cells[currentRow, startOfColumn]];
-            LabelRange.Font.Bold = true;
-        }
-
-        private void AddGridToTable(Excel.Application ExcelApp, ref int currentRow,
-            DataGridView dataGridView, int numberOfWorksheet = 1)
-        {
-            var ws = (Excel.Worksheet)ExcelApp.Worksheets[numberOfWorksheet];
-
-            var rowCount = dataGridView.RowCount;
-            var colCount = dataGridView.ColumnCount;
-
-            MakeBordetForTableCells(ExcelApp, ws, currentRow, rowCount, colCount);
-
-            //storing headers
-            for (int i = 0; i < dataGridView.ColumnCount; i++)
-            {
-                ExcelApp.Cells[currentRow, i + 1] = dataGridView.Columns[i].HeaderText;
-            }
-            currentRow++;
-
-            //storing every cell to excel sheet
-            for (int i = 0; i < dataGridView.RowCount; i++)
-            {
-                for (int j = 0; j < dataGridView.ColumnCount; j++)
-                {
-                    ExcelApp.Cells[i + currentRow, j + 1] = dataGridView[j, i].Value;
-                }
-            }
-
-            currentRow += dataGridView.RowCount;
+            ExcelManager.LabelList = PutTogetherLabels();
+            ExcelManager.GridList = PutTogetherDataGridViews();
+            ExcelManager.Dialog = SaveToExcelDialog;
+            ExcelManager.ExportToExcel();
         }
 
         private List<Label> PutTogetherLabels()
         {
-            var labelList = new List<Label>();
-            labelList.Add(label1);
-            labelList.Add(label2);
-            labelList.Add(label3);
-            labelList.Add(label4);
-            labelList.Add(label5);
-            labelList.Add(label6);
-            labelList.Add(label7);
-            labelList.Add(label8);
-            labelList.Add(label9);
+            var labelList = new List<Label>
+            {
+                label1,
+                label2,
+                label3,
+                label4,
+                label5,
+                label6,
+                label7,
+                label8,
+                label9
+            };
 
             return labelList;
         }
 
         private List<DataGridView> PutTogetherDataGridViews()
         {
-            var gridList = new List<DataGridView>();
-            gridList.Add(first_dataGridView);
-            gridList.Add(second_dataGridView);
-            gridList.Add(third_dataGridView);
-            gridList.Add(fourth_dataGridView);
-            gridList.Add(fifth_dataGridView);
-            gridList.Add(sixth_dataGridView);
-            gridList.Add(seventh_dataGridView);
-            gridList.Add(eighth_dataGridView);
-            gridList.Add(ninth_dataGridView);
-            gridList.Add(tenth_dataGridView);
+            var gridList = new List<DataGridView>
+            {
+                first_dataGridView,
+                second_dataGridView,
+                third_dataGridView,
+                fourth_dataGridView,
+                fifth_dataGridView,
+                sixth_dataGridView,
+                seventh_dataGridView,
+                eighth_dataGridView,
+                ninth_dataGridView,
+                tenth_dataGridView
+            };
 
             return gridList;
-        }
-
-        private void FillTables(Excel.Application ExcelApp)
-        {
-            ExcelApp.Columns.ColumnWidth = 30;
-
-            var currentRow = 1;
-
-            var gridList = PutTogetherDataGridViews();
-            var labelList = PutTogetherLabels();
-
-            for (int i = 0; i < labelList.Count; i++)
-            {
-                AddLabelToTable(ExcelApp, ref currentRow, labelList[i]);
-                AddGridToTable(ExcelApp, ref currentRow, gridList[i]);
-                currentRow += 2;
-            }
-
-            AddGridToTable(ExcelApp, ref currentRow, gridList[9]);
-            currentRow += 2;
-        }
-
-        private void ExportToExcel_Click(object sender, EventArgs e)
-        {
-            SaveToExcelDialog.InitialDirectory = "C:";
-            SaveToExcelDialog.Title = "Save as Excel File";
-            SaveToExcelDialog.FileName = "";
-            SaveToExcelDialog.Filter =
-                "Excel Files(2003)|*.xls|Excel Files(2007)|*.xlsx|Excel Files(2013)|*.xlsx";
-
-            if (SaveToExcelDialog.ShowDialog() != DialogResult.Cancel)
-            {
-                var ExcelApp = new Excel.Application();
-                ExcelApp.Application.Workbooks.Add(Type.Missing);
-
-                FillTables(ExcelApp);
-
-                ExcelApp.ActiveWorkbook.SaveCopyAs(SaveToExcelDialog.FileName.ToString());
-                ExcelApp.ActiveWorkbook.Saved = true;
-                ExcelApp.Quit();
-            }
         }
     }
 }
