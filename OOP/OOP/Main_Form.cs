9ceb79fef6d45d6289d4d2035b05f31140a513af
Buzzon <buzzondev@gmail.com>
2019-04-02 03:19:05 +0500

    рефакторинг

--- OOP/OOP/Main_Form.cs
+++ OOP/OOP/Main_Form.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Windows.Forms;
-using System.Text.RegularExpressions;
-using Excel = Microsoft.Office.Interop.Excel;
 using System.Collections.Generic;
-using System.Globalization;
-//using Application = Microsoft.Office.Interop.Excel.Application;
 
 namespace OOP
 {
@@ -26,36 +22,6 @@ namespace OOP
             Tables = GetADataGridView();
             TableManager.InitializeTables(Tables);
             tableManager = new TableManager(Tables);
-        }
-
-        private DataGridView[] GetADataGridView()
-        {
-            return new DataGridView[] {
-                first_dataGridView, second_dataGridView, third_dataGridView,
-                fourth_dataGridView, fifth_dataGridView, sixth_dataGridView,
-                seventh_dataGridView, eighth_dataGridView, ninth_dataGridView,
-                tenth_dataGridView };
-        }
-
-        private void DataGridView_CellEndEdit(object sender, DataGridViewCellEventArgs e)
-        {
-            tableManager.FillTable((DataGridView)sender);
-        }
-
-        private void Save_send_button_Click(object sender, EventArgs e)
-        {
-            DataManager.Serialize(Tables, TableFileName);
-
-            IPEndPoint address = NetManager.GetAddress(DataManager.ip_info_path);
-            if (!DataManager.ConnectToServer(address))
-                MessageError("Не удалось подключиться к серверу\r\nПопробуйте позже");
-            else
-                DataManager.SendRequest(DataManager.MessageType.SendFile, TableFileName);
-        }
-
-        private void MessageError(string msg)
-        {
-            MessageBox.Show(this, msg, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         private void Main_Form_Load(object sender, EventArgs e)
@@ -83,41 +49,54 @@ namespace OOP
             ExcelManager.ExportToExcel();
         }
 
+        private void Save_send_button_Click(object sender, EventArgs e)
+        {
+            DataManager.Serialize(Tables, TableFileName);
+
+            IPEndPoint address = NetManager.GetAddress(DataManager.ip_info_path);
+            if (!DataManager.ConnectToServer(address))
+                MessageError("Не удалось подключиться к серверу\r\nПопробуйте позже");
+            else
+                DataManager.SendRequest(DataManager.MessageType.SendFile, TableFileName);
+        }
+
+        private void DataGridView_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            tableManager.FillTable((DataGridView)sender);
+        }
+
+        private void MessageError(string msg)
+        {
+            MessageBox.Show(this, msg, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private DataGridView[] GetADataGridView()
+        {
+            return new DataGridView[] {
+                first_dataGridView, second_dataGridView, third_dataGridView,
+                fourth_dataGridView, fifth_dataGridView, sixth_dataGridView,
+                seventh_dataGridView, eighth_dataGridView, ninth_dataGridView,
+                tenth_dataGridView };
+        }
+
         private List<Label> PutTogetherLabels()
         {
-            var labelList = new List<Label>
-            {
-                label1,
-                label2,
-                label3,
-                label4,
-                label5,
-                label6,
-                label7,
-                label8,
-                label9
+            return new List<Label> {
+                label1, label2, label3,
+                label4, label5, label6,
+                label7, label8, label9
             };
-
-            return labelList;
         }
 
         private List<DataGridView> PutTogetherDataGridViews()
         {
-            var gridList = new List<DataGridView>
+            return new List<DataGridView>
             {
-                first_dataGridView,
-                second_dataGridView,
-                third_dataGridView,
-                fourth_dataGridView,
-                fifth_dataGridView,
-                sixth_dataGridView,
-                seventh_dataGridView,
-                eighth_dataGridView,
-                ninth_dataGridView,
+                first_dataGridView, second_dataGridView, third_dataGridView,
+                fourth_dataGridView, fifth_dataGridView, sixth_dataGridView,
+                seventh_dataGridView, eighth_dataGridView, ninth_dataGridView,
                 tenth_dataGridView
             };
-
-            return gridList;
         }
     }
 }
